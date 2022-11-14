0d58dc4d6227345f427f019d020dd0834c53f585
FabioMuriel <fabiomurielvega16@gmail.com>
2022-11-14 17:16:29 -0500

    Nuevos cambios

---
+++ Operadores.dart
@@ -0,0 +1,13 @@
+void main() {
+  int suma = 5 + 7 + 8;
+  double resta = 5 - 6 - 7 - 8;
+  int multiplicacion = 5 * 6 * 7 * 8;
+  double division = 7 / 7;
+  double modulo = 7 % 7;
+
+  print(suma);
+  print(resta);
+  print(multiplicacion);
+  print(division);
+  print(modulo);
+}
